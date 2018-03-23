f4c97f020c739de8c484ee15579ce12d1eb1223e
Erika Gutierrez <erikagutierrez@Erikas-MacBook-Air.local>
2018-03-23 00:55:54 -0700

    v2

--- stock_app.py
+++ stock_app.py
@@ -6,20 +6,34 @@ import pandas as pd
 
 import numpy as np
 from bokeh.layouts import gridplot
-from bokeh.plotting import figure, show, output_file
+from bokeh.plotting import figure, output_file
+from bokeh.resources import CDN
+from bokeh.embed import file_html
+from bokeh.embed import components
 
+from datetime import date, timedelta
+from monthdelta import monthdelta
 
 app = Flask(__name__)
 
+@app.route('/stock_ticker')
+def stock_ticker():
 
-@app.route('/')
-def home():
     '''receive user input through flask site'''
-    user_input = 'AMZN'
+    user_input = request.args.get('ticker').upper()
+    if user_input == '' :
+        user_input = 'AMZN'
 
     '''Quandl API request'''
-    ticker = 'https://www.quandl.com/api/v3/datasets/WIKI/AAPL?trim_start=2016-01-01&trim_end=2018-03-05&api_key=v1eCq4j6XirYCp4gU9VJ'
+    yesterday = date.today() - timedelta(days=1)
+    last_month = yesterday - monthdelta(1)
+    yesterday = yesterday.strftime('%Y-%m-%d')
+    last_month = last_month.strftime('%Y-%m-%d')
+
+    ticker = 'https://www.quandl.com/api/v3/datasets/WIKI/AAPL?trim_start=2018-02-15&trim_end=2018-03-23&api_key=v1eCq4j6XirYCp4gU9VJ'
     ticker = ticker.replace('AAPL', user_input)
+    ticker = ticker.replace('2018-02-15', last_month)
+    ticker = ticker.replace('2018-03-23', yesterday)
 
     r = requests.get(ticker)
 
@@ -28,36 +42,41 @@ def home():
 
     #these are the columns names:
     # ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Ex-Dividend', 'Split Ratio', 'Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']
-    columns = d['dataset']['column_names']
+    try:
+        columns = d['dataset']['column_names']
 
-    data = d['dataset']['data']
+        data = d['dataset']['data']
 
-    '''create pandas df'''
-    df = pd.DataFrame(data, columns=columns)
-
-
-    '''create bokeh figure'''
-    def datetime(x):
-        return np.array(x, dtype=np.datetime64)
-
-    p1 = figure(x_axis_type="datetime", title="Stock Closing Prices")
-    p1.grid.grid_line_alpha=0.3
-    p1.xaxis.axis_label = 'Date'
-    p1.yaxis.axis_label = 'Price'
-
-    p1.line(datetime(df['Date']), df['Close'], color='#A6CEE3', legend=user_input)
-    p1.legend.location = "top_left"
-
-    output_file("stocks.html", title="stocks.py example")
+        '''create pandas df'''
+        df = pd.DataFrame(data, columns=columns)
 
 
+        '''create bokeh figure'''
+        def datetime(x):
+            return np.array(x, dtype=np.datetime64)
+
+        p1 = figure(x_axis_type="datetime", title="Stock Closing Prices")
+        p1.grid.grid_line_alpha=0.3
+        p1.xaxis.axis_label = 'Date'
+        p1.yaxis.axis_label = 'Price'
+
+        p1.line(datetime(df['Date']), df['Close'], color='#A6CEE3', legend=user_input)
+        p1.legend.location = "top_left"
+
+        fname = 'user_input_stock_ticker.html'
+        # html = file_html(p1, CDN, fname)
+
+        script, div = components(p1)
+        html = script + div
+
+    except :
+        html = 'No such company.'
+    return html
+
+@app.route('/')
+def home():
     return render_template('home.html')
 
-
-
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
 
 if __name__ == '__main__':
   app.run()
